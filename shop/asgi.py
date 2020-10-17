9253dd053602201342bc2d34731b960d772a5793
Vyacheslav Krachkov <v.krachkov@enestech.net>
2020-10-18 00:23:42 +0300

    new: add 'fastapi' support

--- shop/asgi.py
+++ shop/asgi.py
@@ -1,7 +1,17 @@
 import os
 
 from django.core.asgi import get_asgi_application
+from fastapi import FastAPI
+from starlette import routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shop.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "shop.settings")
 
-application = get_asgi_application()
+django_application = get_asgi_application()
+fastapi_application = FastAPI()
+
+application = routing.Router(
+    [
+        routing.Mount("/api", app=fastapi_application),
+        routing.Mount("/", app=django_application),
+    ]
+)
